face2bc1779473063e7020bfeaf98cc4adb0593b
Nathan Fassnacht <57467493+Nathdoesmath@users.noreply.github.com>
2022-09-23 11:32:54 -0400

    More pain related to the discord sdk

--- DND-Panel/MainWindow.xaml.cs
+++ DND-Panel/MainWindow.xaml.cs
@@ -22,19 +22,33 @@ namespace DND_Panel {
         public const string VersionNum = "0.1.0";
         
         // Object to access the Discord api
-        public Discord.Discord discord = new Discord.Discord(NoCommit.CLIENT_ID, (UInt64) Discord.CreateFlags.Default);
+        public Discord.Discord discord;
+        public Discord.ActivityManager aM;
+
 
         // The time that the application was opened
-        public long startTime = DateTime.UnixEpoch.Ticks;
+        public long startTime;
 
         // Object for the exe window
         public MainWindow()
         {
+
             // Creates the window with the hard coded XAML file
             InitializeComponent();
 
             // Sets the window title dynamically to easily show the version number
             this.Title = $"DND Panel | {VersionNum}";
+
+            this.discord = new Discord.Discord(NoCommit.CLIENT_ID, (UInt64)Discord.CreateFlags.NoRequireDiscord);
+            
+            discord.SetLogHook(Discord.LogLevel.Debug, (level, message) =>
+            {
+                consoleOutput.Text += $"Log[{level}] {message}\n";
+            });
+
+            this.aM = discord.GetActivityManager();
+
+            this.startTime = DateTime.Now.Ticks;
 
             // starts an asyncronous loop to update the Rich Presence
             updateLoop();
@@ -44,7 +58,9 @@ namespace DND_Panel {
         void MinWindow_Closing(object sender, CancelEventArgs e)
         {
             // Clears the game activity when the game is closed
-            discord.ActivityManagerInstance.ClearActivity(result => { });
+            aM.ClearActivity(result => {
+                consoleOutput.Text += result;
+            });
         }
 
         // Updates Discord Rich presence
@@ -52,41 +68,68 @@ namespace DND_Panel {
         {
             Discord.Activity activity = new Discord.Activity
             {
-                State = "DND",
-                Details = "",
+                State = $"DND | {VersionNum}",
+                Details = "-=-=-",
                 Timestamps = {
                       Start = startTime,
                   },
                 Assets = {
                         // Large Image name from Discord developer portal
-                      LargeImage = "awaiting logo",
+                      LargeImage = "test",
                         // Hovered text for Large Image
                       LargeText = $"DND Panel | {VersionNum}",
                         // Corner Icon name from Discord dev portal
-                      SmallImage = "need heart image",
+                      SmallImage = "test",
                         // Hovered text for the corner icon
                       SmallText = $"HP: {"not implemented"}",
                   },
-                Party = {
-                      Id = "foo partyID",
-                      Size = {
-                          CurrentSize = 1,
-                          MaxSize = 4,
-                      },
+                Party =
+                  {
+                    Id = "6942069420",
+                    Size = 
+                    {
+                        CurrentSize = 1,
+                        MaxSize = 4,
+                    },
                   },
-                Secrets = {},
+                Secrets =
+                  {},
                 Instance = true,
             };
-            discord.ActivityManagerInstance.UpdateActivity(activity, result => {});
+
+            consoleOutput.Text += "attempting update\n";
+            try
+            {
+                consoleOutput.Text += "Debug1\n";
+
+                aM.UpdateActivity(activity, result =>
+                {
+                    if (result == Discord.Result.Ok)
+                    {
+                        consoleOutput.Text += $"updated\t{DateTime.Now.Ticks - startTime}\n";
+                    } else
+                    {
+                        consoleOutput.Text += "Failed to update";
+                    }
+                consoleOutput.Text += "Debug2\n";
+                });
+
+                consoleOutput.Text += "Debug3\n";
+
+            } catch (Exception e)
+            {
+                consoleOutput.Text += "Debug4\n";
+                consoleOutput.Text += e.ToString();
+            }
         }
 
-        // Updates the Discord Rich Presence status every 4 seconds
+        // Updates the Discord Rich Presence status every 5 seconds
         async private void updateLoop()
         {
             while (true)
             {
-                await Task.Delay(4000);
                 updateRP();
+                await Task.Delay(5000);
             }
         }
     }
